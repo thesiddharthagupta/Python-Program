b078ee9bc0ac1ee66f7eb36c00e0be98354bb3d9
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-10 02:07:17 +0530

    multification table

---
+++ LoopsInPython/Multification_table.py
@@ -0,0 +1,22 @@
+'''
+
+Print the multiplication table of a number
+'''
+
+''' 
+by for loops
+
+n = int(input("Enter a number: "))
+for i in range (1,11):
+    print(f" {n} x {i} = {n * i}")
+
+
+'''
+
+n = int(input("Enter a number: "))
+i = 1
+while (i<11):
+    print(f" {n} x {i} = {n * i}")
+    i += 1
+   
+    
