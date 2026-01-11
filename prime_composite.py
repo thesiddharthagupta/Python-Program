3fde2f75d5489c6508f4a7d297635fa1216866c1
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-12 03:16:38 +0530

    update Prime composite check

--- prime_composite.py
+++ prime_composite.py
@@ -1,3 +1,15 @@
-num = int(input("Enter any nummber: "))
-count = 0
-n % 2 and n % n == 0 
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Neither prime nor composite")
+else:
+    count = 0
+
+    for i in range(2, num):
+        if num % i == 0:
+            count += 1
+
+    if count == 0:
+        print("Prime number")
+    else:
+        print("Composite number")
