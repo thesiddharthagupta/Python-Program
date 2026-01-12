59824b6db182fecdd1a17d0f6f8e1357905db3e9
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-13 01:52:04 +0530

    average of 3 numbers

---
+++ Function_in_Python/Avg.py
@@ -0,0 +1,14 @@
+def avg():
+    n1 = int(input("ENter 1st number: "))
+    n2 = int(input("ENter 2nd number: "))
+    n3 = int(input("ENter 3rd number: "))
+
+    avg = n1 + n2 + n3 / 3
+    print("the average is: ", avg)
+    print(avg)
+
+avg()   #1st call
+avg()   #2nd call
+
+
+
