b824a1f5d86b5b9c18db235e5ef138ad6f1b44c4
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-14 23:14:41 +0530

    List in python

---
+++ ListInPython/List.py
@@ -0,0 +1,5 @@
+marks = [94.4, 23.1, 56.89, 25.12, 45.2]
+print(marks)            #list are mutable means which can change
+print (type(marks))    # type is used for to know the type of marks
+print(marks[0])     #for the index of marks
+print(len(marks))       #for the length of marks 
