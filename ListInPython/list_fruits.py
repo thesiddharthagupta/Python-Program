703b3850cb138da619567da489487a9f3bcf5b10
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-15 22:44:18 +0530

    list of fruits

---
+++ ListInPython/list_fruits.py
@@ -0,0 +1,14 @@
+fruits = []
+
+f1 = input("ENter fruits name: ")
+fruits.append(f1)
+f2 = input("ENter fruits name: ")
+fruits.append(f2)
+f3 = input("ENter fruits name: ")
+fruits.append(f3)
+f4 = input("ENter fruits name: ")
+fruits.append(f4)
+f5 = input("ENter fruits name: ")
+fruits.append(f5)
+
+print(fruits)
