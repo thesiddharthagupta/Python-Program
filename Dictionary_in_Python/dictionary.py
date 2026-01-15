73397ff5dd82840fa374de53a07c0db42c69301e
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-16 00:15:34 +0530

    update

--- Dictionary_in_Python/dictionary.py
+++ Dictionary_in_Python/dictionary.py
@@ -5,3 +5,12 @@ dict = {
 }
 
 print(dict.items())
+print(dict.get("key"))  # to get the value
+
+dict.update({"city" : "bangloure"})  #update
+print(dict)
+
+new_dict = {"Science" : "100", "age" : 17}  #update the dict
+dict.update(new_dict)  #for the new dict
+
+print(dict)
