785e0a7d47990c1aca1d27bcd627185d60c85ccd
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-15 22:39:48 +0530

    nasted Dictionary

---
+++ Dictionary_in_Python/nasted_dict.py
@@ -0,0 +1,11 @@
+data = {
+    "name": "Siddharth",
+    "subjects": {
+        "phy": 98,
+        "chem": 97,
+        "math": 99
+    }
+}
+
+print(data["name"])              # Siddharth
+print(data["subjects"]["math"])  # 99
