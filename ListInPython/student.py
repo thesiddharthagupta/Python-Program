79c7629d088d69775648e9fa3bd35181743bc26a
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-15 22:46:46 +0530

    Students of list

---
+++ ListInPython/student.py
@@ -0,0 +1,18 @@
+marks = []
+
+m1 = int(input("ENter marks here: "))
+marks.append(m1)
+m2 = int(input("ENter marks here: "))
+marks.append(m2)
+m3 = int(input("ENter marks here: "))
+marks.append(m3)
+m4 = int(input("ENter marks here: "))
+marks.append(m4)
+m5 = int(input("ENter marks here: "))
+marks.append(m5)
+
+marks.sort()
+print(marks)
+
+
+
