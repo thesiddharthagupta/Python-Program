a5ef8aa80d8a60e431bdf39e139a6f2038b9d606
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-16 23:29:46 +0530

    Sets methods

--- Sets_in_Python/sets_method.py
+++ Sets_in_Python/sets_method.py
@@ -1,12 +1,12 @@
 collection = set()   #sets element are immutalble but sets are mutalble
 
-collection.add(1)
+collection.add(1)       #to add in set
 print(collection)
 
 collection.add(5)
 print(collection)
 
-collection.add("siddharth")
+collection.add("siddharth") #to add in set
 print(collection)
 
 
@@ -14,18 +14,18 @@ collection.add((1,2,4,25))  #tuples can be add but list cannt bxoz list are muta
 print(collection)
 
 
-collection.add(2)
+collection.add(2)       #to add inn set
 print(collection)
 
 
-collection.remove(2)
+collection.remove(2)    #to remove from set
 print(len(collection))
 
 collection.pop()    #its pop the randome value from set
 print(collection)
 
-collection.clear()
-print(len(collection))
+collection.clear()      #to clean the set completely
+print(len(collection))  #to print length
 
 
 
