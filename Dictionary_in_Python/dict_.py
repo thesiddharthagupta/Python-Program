a4104cf24c34e55423cb877f9199ed947ab4bab1
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-16 18:55:18 +0530

    Dictionary

---
+++ Dictionary_in_Python/dict_.py
@@ -0,0 +1,15 @@
+marks = {}
+
+x = int(input("ENter phy marks: "))
+marks.update({"phy ": x})
+
+x = int(input("ENter chem marks: "))
+marks.update({"chem ": x})
+
+x = int(input("ENter math marks: "))
+marks.update({"math ": x})
+
+x = int(input("ENter scnc marks: "))
+marks.update({"scnc ": x})
+
+print(marks)
