c2cc6eab2c665b4fdf28936408ffa05886f6c8ab
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-20 22:43:13 +0530

    table

---
+++ File_io_in_Python/game.py
@@ -0,0 +1,24 @@
+import random
+
+def game():
+    print("you are playing the game")
+    score = random.randint(1, 62)
+    #fetch the highscore
+    return random.randint(1,62)
+    with open("highScore.txt") as f:
+        highscore = f.read()
+        if (highscore != ""):
+            highscore = int(highscore)
+        else:
+            highscore = 0
+
+    print(f"your score:  {score}")
+    if (score>highscore):
+        with open("highscore.txt", "w") as f:
+            f.write(str(score))
+
+    return score
+
+game()
+
+
