a652b565a023f6e0a5a141696a1a3d1d8e017ec0
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-23 21:35:54 +0530

    greater among 3

---
+++ Conditional_Statement/spam.py
@@ -0,0 +1,12 @@
+n1 = int(input("ENter a number: "))
+n2 = int(input("ENter a number: "))
+n3 = int(input("ENter a number: "))
+
+if n1 > n2 and n1 > n3:
+    print("the Greater number is :", n1)
+elif n2 > n1 and n2 > n3:
+    print("greater number is : ", n2)
+else:
+    print("the greater number is : ", n3)
+
+
