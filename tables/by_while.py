b20944d3e602278db2ad693460b3eedb505a33dc
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-28 23:42:49 +0530

    while loops

---
+++ tables/by_while.py
@@ -0,0 +1,22 @@
+# by parameter pasing as "a"
+num = int(input("Enter a number: "))
+
+def table(a):
+    i = 1
+    while i <= 10:   # loop runs until i reaches 10
+        print(f"{a} X {i} = {a * i}")
+        i += 1       # increment i each time
+
+table(num)
+
+
+
+# by without parameter
+
+# num = int(input("ENter a number: "))
+# def table(num):
+#     i = 1
+#     while i<=10:      # loop runs until i reaches 10
+#         print(f"{num} X  {i} = {num*i}")
+#         i += 1        # increment i each time
+# table(num)
