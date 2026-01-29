2c83a52ef29d17338963657e1a02fd976adc0f2e
thesiddharthagupta <thesiddgupta@gmail.com>
2026-01-29 15:10:26 +0530

    Maximum number by its build in function

---
+++ Tuples_in_Python/max_2.py
@@ -0,0 +1,8 @@
+num = (1,2,9,16,28,25,36,49)
+
+maximum = max(num)   #for direct maximum number:
+minimum = min(num)  #for direct minimum number by "Build in function"
+
+print(type(num))  #for typr of num
+print("the maximum number is :", max)
+print("The minimum number is :", min)
