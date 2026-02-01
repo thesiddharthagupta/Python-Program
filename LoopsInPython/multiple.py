d1271fa1b0e6417ca45e9e6bdf935083b7d79108
thesiddharthagupta <thesiddgupta@gmail.com>
2026-02-01 21:56:10 +0530

    Multiple

---
+++ LoopsInPython/multiple.py
@@ -0,0 +1,14 @@
+i = 100
+print("> Multiple of 5")
+while i <= 200:
+    if i % 2  == 0 and i % 5 == 0:
+        print(i)
+    i += 1
+
+i = 100
+print("> Multiple of 7")
+
+while i <= 200:
+    if i % 2 == 0 and i % 7 == 0:
+        print(i)
+    i += 1
