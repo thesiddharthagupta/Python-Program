5bebf10052ccb6e584d6186664635aa741383860
thesiddharthagupta <thesiddgupta@gmail.com>
2026-02-04 23:53:46 +0530

    Greater input  number

--- Conditional_Statement/spam.py
+++ Conditional_Statement/spam.py
@@ -6,7 +6,10 @@ if n1 > n2 and n1 > n3:
     print("the Greater number is :", n1)
 elif n2 > n1 and n2 > n3:
     print("greater number is : ", n2)
-else:
+elif n3 > n1 and n3 > n2:
     print("the greater number is : ", n3)
+else:
+    print("Invalid number, please Enter again!")
+
 
 
