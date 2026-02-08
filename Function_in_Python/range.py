e3890390770004e8991775ae3eeea7c829d2d9fa
thesiddharthagupta <thesiddgupta@gmail.com>
2026-02-08 23:52:15 +0530

    range print

---
+++ Function_in_Python/range.py
@@ -0,0 +1,9 @@
+
+
+l = [1, 9, 7, "alish", "tom", "bob"]
+
+def print_list(lst):
+    for i in range(len(l)):   # i = 0,1,2,3,4,5
+        print(lst[i])           # print element at index i
+
+print_list(l)
