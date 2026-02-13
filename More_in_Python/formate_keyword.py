06328cf3b5c7fa58943f26c61468dd72d745360a
thesiddharthagupta <thesiddgupta@gmail.com>
2026-02-13 23:57:55 +0530

    formate keyword

--- More_in_Python/formate_keyword.py
+++ More_in_Python/formate_keyword.py
@@ -3,5 +3,5 @@ marks = int(input("Enter marks: "))
 phone = int(input("Enter phone number: "))
 
 dairy = "the name of the student is {}, with marks are {} and phone number is {}".format(name, marks, phone)
-
+#used for formating the values.
 print(dairy)
