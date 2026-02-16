26499053a9c8d3bda28be482213102372d7860ea
thesiddharthagupta <thesiddgupta@gmail.com>
2026-02-16 11:28:15 +0530

    search

--- File_io_in_Python/search.py
+++ File_io_in_Python/search.py
@@ -5,3 +5,9 @@ if("python" in content):
     print("yes, python is present in text")
 else:
     print("No, Python is not present in text")
+
+
+with open(r"File_io_in_Python\replace.txt", "r") as file:
+    for i, line in enumerate(file, start=1):
+        if "python" in line.lower():
+            print("Python found at line:", i)
