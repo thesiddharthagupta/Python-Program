94b2d4612871efc4b7629e213252bfccd8253cc8
thesiddharthagupta <thesiddgupta@gmail.com>
2026-02-19 11:47:40 +0530

    exception

--- More_in_Python/exception.py
+++ More_in_Python/exception.py
@@ -4,7 +4,6 @@ try:
 except Exception as e:
     print(e)
 
-
 try:
     with open(r"E:\Scratch.python\More_in_Python\2.txt", "r") as f: #here file is created so i have pasted the file link with 'r' bcoz of \2. <- excape sequence.
         print(f.read())
